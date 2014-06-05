a0cd1b103a311aa984cf70eeeb0260a95d1829bb
DuckyCho <zootyducky@gmail.com>
2014-06-05 10:28:12 +0900

    0605 10:28

--- memberManagementSystem.h
+++ memberManagementSystem.h
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 #include <Windows.h>
+
+
 #define basicStringSize 80
 #define black 0
 #define red 1
--- redBlacktree_insertion.c
+++ redBlacktree_insertion.c
@@ -116,6 +116,8 @@ void rotateRight(member* father, member * grandF, rootPointer * RP)
 	member * node = father->left;
 	member * rightChildOfNode = node->right;
 
+
+
 	if (grandF != NULL)
 	{
 		if (node->id > grandF->id)
